93ade8f457e86d5270ee272e5a9595b700233a48
User <User@DESKTOP-4GL677D.wifi.uc.edu>
2022-10-20 09:10:51 -0400

    Assignment09 Main and Dessert - AR

---
+++ Dori_Assignment09/DessertPackage/CupcakeClass.py
@@ -0,0 +1,33 @@
+'''
+Created on Oct 20, 2022
+
+@author: User
+'''
+#We need to identify the class
+class Cupcake():
+
+#This sets the price of a cupcake 
+    def setPrice(self, price):
+        self.validatePrice(price)
+#This validates the price before we store it in the object
+    def validatePrice(self, price):
+        if price < 0: 
+            print ("Cupcakes are not free")
+        else: 
+            self.price = price 
+            
+#This is the int
+    def __init__ (self, flavor, price):  # we are ensuring that an cupcake cannot be instantiated without a flavor
+        self.flavor = flavor # store the flavor in the current object 
+        self.validatePrice(price)
+    
+    def __repr__(self):  # will return string representation of the object
+        """
+        return a string represenation of the object 
+        """
+        return "flavor = " + self.flavor 
+    def __str__(self):  #does pretty much the same thing as repr
+        """
+        return a 'pretty' string presentaion of the Object
+        """
+        return "flavor = " + self.flavor + " price =" + str(self.price)
---
+++ Dori_Assignment09/MainPackage/Main.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 20, 2022
+
+@author: User
+'''
+from DessertPackage.CupcakeClass import *
+from DessertPackage.SkittlesClass import *
+from DessertPackage.BrowniesClass import *
+
+cupcake = Cupcake("vanilla", 2)
+print(cupcake.__str__())
+
+print(cupcake.flavor)
+
+#Negative Test
+cupcake.setPrice(-3)
+#Positive Test
+cupcake.setPrice(3)
+print(cupcake.__str__())

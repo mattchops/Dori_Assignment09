93d69dc9c1a56c1abc1ed98734605e0fb3804b41
User <User@DESKTOP-SLKFG33.lan>
2022-10-24 20:26:39 -0400

    Added Credentials to the Main

--- Dori_Assignment09/MainPackage/Main.py
+++ Dori_Assignment09/MainPackage/Main.py
@@ -1,8 +1,16 @@
 '''
-Created on Oct 20, 2022
-
-@author: User
+Created on Oct 24, 2022
+Name: Amanda Rapien, Aaron Paulson, Ben Truax, Matthew Lamb
+email: Lambmj@mail.uc.edu, rapienaa@mail.uc.edu, paulsoar@mail.uc.edu,truaxbp@mail.uc.edu
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that we can develop an Eclipse project 
+modeling something in the real world, and collaborate on GitHub.
+Citations: N/A
+Anything else that's relevant:
 '''
+
 from DessertPackage.CupcakeClass import *
 from DessertPackage.SkittlesClass import *
 from DessertPackage.BrowniesClass import *

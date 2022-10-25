abaebd84070fca8a3223a99e769464e5b5cad45d
User <User@DESKTOP-SLKFG33.lan>
2022-10-24 20:11:23 -0400

    Added Cupcake Class

--- Dori_Assignment09/DessertPackage/CupcakeClass.py
+++ Dori_Assignment09/DessertPackage/CupcakeClass.py
@@ -1,7 +1,14 @@
 '''
-Created on Oct 20, 2022
-
-@author: User
+Created on Oct 24, 2022
+Name: Amanda Rapien, Aaron Paulson, Ben Truax, Matthew Lamb
+email: Lambmj@mail.uc.edu, rapienaa@mail.uc.edu, paulsoar@mail.uc.edu,truaxbp@mail.uc.edu
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that we can develop an Eclipse project 
+modeling something in the real world, and collaborate on GitHub.
+Citations: N/A
+Anything else that's relevant:
 '''
 #We need to identify the class
 class Cupcake():
